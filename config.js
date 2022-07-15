05391432206ab82f8fe572a2819e3dfe5fe76f8f
jonkir314e <109366032+jonkir314e@users.noreply.github.com>
2022-07-16 00:03:23 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,14 +3,14 @@ const CONFIG = {
     introTitle: 'the most beautiful girl',
     introDesc: `Đây là điều ước cuối cùng của mèo dành cho Đon nè, 
     từ nay mèo sẽ đi nghỉ hưu không còn hỗ trợ Đon đc nữa rùi`,
-    btnIntro: 'chạm vào tui đi',
-    title: 'từ mai là Đoan sẽ bắt đầu hành trình mới của mình rồi ',
-    title: 'nên là mèo ước cho Đoan có 1 cuộc sống vui vẻ, hạnh phúc và gặp nhiều may mắn🥰.Cố lên nha!!!',
+    btnIntro1: 'chạm vào tui đi',
+    title1: 'từ mai là Đoan sẽ bắt đầu hành trình mới của mình rồi ',
+    title2: 'nên là mèo ước cho Đoan có 1 cuộc sống vui vẻ, hạnh phúc và gặp nhiều may mắn🥰.Cố lên nha!!!',
     desc: 'nếu không ngại thì cứ alo cho người đó(Đon biết là ai mà;D),người đó sẽ đến ngay UwU ',
-    btnIntro: 'chạm vào tui lần nữa đi',
-    title: 'À quên nữa, dù mèo đi xa rồi nhưng mèo sẽ để lại trách nhiệm này cho người đó, 1 người mà sẽ luôn sẵn sàng giúp đỡ mỗi khi Đon cần😘 ',
-    btnIntro: 'thêm 1 lần nữa ;)',
-    title: 'nếu Đon đã sẵn sàng trả lời thì hãy đi tiếp',
+    btnIntro2: 'chạm vào tui lần nữa đi',
+    title3: 'À quên nữa, dù mèo đi xa rồi nhưng mèo sẽ để lại trách nhiệm này cho người đó, 1 người mà sẽ luôn sẵn sàng giúp đỡ mỗi khi Đon cần😘 ',
+    btnIntro3: 'thêm 1 lần nữa ;)',
+    title4: 'nếu Đon đã sẵn sàng trả lời thì hãy đi tiếp',
     btnYes: 'nhấn vô đây nếu Đon chưa sẵn sàng',
     question: 'vậy thì tui vẫn sẽ chờ Đon và Đon có mún chúc tui gì hong nè',
     btnReply: 'Gửi cho bạn <3',
@@ -20,3 +20,4 @@ const CONFIG = {
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
+
